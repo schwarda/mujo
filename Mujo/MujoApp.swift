a0a1a7631b3272c473e2985993c5b3c2c88249c3
schwarda <schwartzd14@gmail.com>
2026-09-05 19:23:53 +0200

    Initial Commit

---
+++ Mujo/MujoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MujoApp.swift
+//  Mujo
+//
+//  Created by Lopk Art on 05/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct MujoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MujoTests/MujoTests.swift
@@ -0,0 +1,19 @@
+//
+//  MujoTests.swift
+//  MujoTests
+//
+//  Created by Lopk Art on 05/09/2026.
+//
+
+import Testing
+@testable import Mujo
+
+struct MujoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
